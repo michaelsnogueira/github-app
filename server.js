a6e75f0071f105aaf7fcaa0e6ce6ddbed05c51e3
michaelsnogueira <michaelsnogueira@gmail.com>
2017-11-22 09:15:12 -0200

    Versao 0.1

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: { colors:true }
+}).listen(6000, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+  console.log('Listening on http://localhost:6000')
+})
